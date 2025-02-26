f1c4190ac7d22e378f47061e7d4c0020a07c36bd
Javad Khalilian <khalilian.javad@gmail.com>
2025-02-26 17:00:06 +0100

    fix: script

---
+++ packages/solidity-ts-example/scripts/deploy.ts
@@ -0,0 +1,22 @@
+import { chainweb } from 'hardhat';
+
+async function main() {
+  const deployed = await chainweb.deployContractOnChains('SimpleToken');
+
+  if (deployed.deployments.length === 0) {
+    console.log('No contracts deployed');
+    return;
+  }
+  console.log('Contracts deployed');
+
+  deployed.deployments.forEach(async (deployment) => {
+    console.log(`${deployment.address} on ${deployment.chain}`);
+  });
+}
+
+main()
+  .then(() => process.exit(0)) // Exiting the process if deployment is successful.
+  .catch((error) => {
+    console.error(error); // Logging any errors encountered during deployment.
+    process.exit(1); // Exiting the process with an error code.
+  });
