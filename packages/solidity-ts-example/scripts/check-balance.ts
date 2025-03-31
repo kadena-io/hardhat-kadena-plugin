88b91390db1eabb57d7ef2f98358e3d3fb665c89
Heather S <42716568+hswopeams@users.noreply.github.com>
2025-03-31 14:46:54 +0200

    Add comments

--- packages/solidity-ts-example/scripts/check-balance.ts
+++ packages/solidity-ts-example/scripts/check-balance.ts
@@ -1,5 +1,13 @@
 import { chainweb, ethers } from 'hardhat';
 
+/** This script checks the ETH balance of a specific wallet on two different forked chainweb chains.
+  * It is meant to test forking of Ethereum mainnet. Other chains can be forked the same way.
+  * An external node should be started uisng `npx hardhat node` with the forking config in hardhat.config.ts
+  * An alternative is to remove the forking config from hardhat.config.ts and start the node using `npx hardhat node --fork https://eth.drpc.org``
+  * You can use any RPC URL for the forking, but the one used in this example is a public RPC URL for Ethereum mainnet.
+  * Run the script uisng the command `npx hardhat run scripts/check-balance.ts --chainweb localhost`
+  * Note the use of chainweb.switchChain to switch between the two chains to get the balance
+*/
 async function main() {
   console.log("Checking ETH balance...");
 
