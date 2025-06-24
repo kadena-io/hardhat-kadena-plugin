25b2fd86174345b3559ee0e227880d1642f586f6
Edmund Noble <edmundnoble@gmail.com>
2025-06-24 18:46:08 -0400

    put new precompile into hardhat

--- packages/hardhat-kadena/src/utils/chain.ts
+++ packages/hardhat-kadena/src/utils/chain.ts
@@ -7,6 +7,8 @@ import {
   CHAIN_ID_BYTE_CODE,
   VERIFY_ADDRESS,
   VERIFY_BYTE_CODE,
+  REDEEM_ADDRESS,
+  REDEEM_BYTE_CODE,
 } from './network-contracts.js';
 import { EthereumProvider, KadenaNetworkConfig } from 'hardhat/types';
 import { COLOR_PALETTE, logError, Logger, logInfo } from './logger.js';
@@ -147,6 +149,13 @@ export class Chain {
     ]);
   }
 
+  async initializeRedeemPrecompile() {
+    await this.provider.send('hardhat_setCode', [
+      REDEEM_ADDRESS,
+      REDEEM_BYTE_CODE,
+    ]);
+  }
+
   async enableAutomine() {
     if (!this.autominer) {
       this.autominer = setInterval(() => this.runPending(), 100);
@@ -177,6 +186,7 @@ export class Chain {
     // initialize system contracts
     await this.initializeCidContract();
     await this.initializeVerificationPrecompile();
+    await this.initializeRedeemPrecompile();
 
     // setup automining
     await this.provider.send('evm_setAutomine', [false]);
