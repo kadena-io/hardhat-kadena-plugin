63538b8e1ca503ad9b509d0596cffa13eb885fb2
Heather S <42716568+hswopeams@users.noreply.github.com>
2025-07-08 16:15:56 +0200

    Format and lint

--- packages/solidity-example/scripts/deploy.js
+++ packages/solidity-example/scripts/deploy.js
@@ -85,7 +85,10 @@ async function main() {
           console.log(`Attempting to verify contract on chain ${chainId}...`);
           await run('verify:verify', {
             address: contractAddress,
-            constructorArguments: [ethers.parseUnits('1000000'), deployer.address],
+            constructorArguments: [
+              ethers.parseUnits('1000000'),
+              deployer.address,
+            ],
             force: true,
           });
 
